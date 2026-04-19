4bed740b1eb1f64573bb4df2339ad3baf03648a7
ss <sasha69.tmk@gmail.com>
2026-04-19 19:19:52 +0300

    Добавлен базовый класс ViewModelBase

---
+++ HardwareMonitor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HardwareMonitor
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaiseChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string name = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            RaiseChanged(name);
+            return true;
+        }
+    }
+}
