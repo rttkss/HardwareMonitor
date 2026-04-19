08c93b214d09d78a1c69f114ac4289c7ad77592e
ss <sasha69.tmk@gmail.com>
2026-04-19 19:20:14 +0300

    Добавлены модели накопителей StorageData, DriveUnit и Partition

---
+++ HardwareMonitor/Models/StorageData.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace HardwareMonitor
+{
+    public class StorageData
+    {
+        public List<DriveUnit> Drives { get; set; }
+        public List<Partition> Partitions { get; set; }
+
+        public StorageData()
+        {
+            Drives = new List<DriveUnit>();
+            Partitions = new List<Partition>();
+        }
+    }
+
+    public class DriveUnit
+    {
+        public string ModelName { get; set; }
+        public long TotalBytes { get; set; }
+        public string Type { get; set; }
+
+        public DriveUnit()
+        {
+            ModelName = "Не определено";
+            Type = "Не определено";
+        }
+
+        public string SizeText => $"{TotalBytes / 1024.0 / 1024 / 1024:F1} ГБ";
+    }
+
+    public class Partition
+    {
+        public string Letter { get; set; }
+        public long CapacityBytes { get; set; }
+        public long FreeBytes { get; set; }
+        public string FsType { get; set; }
+
+        public double OccupiedPercent
+        {
+            get
+            {
+                if (CapacityBytes <= 0) return 0;
+                return (double)(CapacityBytes - FreeBytes) / CapacityBytes * 100.0;
+            }
+        }
+
+        public long UsedBytes => CapacityBytes - FreeBytes;
+
+        public Partition()
+        {
+            Letter = "?";
+            FsType = "Не определено";
+        }
+    }
+}
