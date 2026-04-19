ed31235ec891190da86534f0e0815540427bfbf5
ss <sasha69.tmk@gmail.com>
2026-04-19 19:21:32 +0300

    Добавлен сервис мониторинга оперативной памяти

---
+++ HardwareMonitor/Services/RamService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Management;
+
+namespace HardwareMonitor
+{
+    public class RamService
+    {
+        public RamData Collect()
+        {
+            RamData data = new RamData();
+
+            try
+            {
+                ManagementObjectSearcher osSearcher = new ManagementObjectSearcher(
+                    "SELECT TotalVisibleMemorySize, FreePhysicalMemory FROM Win32_OperatingSystem");
+
+                foreach (ManagementObject item in osSearcher.Get())
+                {
+                    long totalKB = Convert.ToInt64(item["TotalVisibleMemorySize"] ?? 0);
+                    long freeKB = Convert.ToInt64(item["FreePhysicalMemory"] ?? 0);
+                    data.TotalBytes = totalKB * 1024;
+                    data.FreeBytes = freeKB * 1024;
+                    break;
+                }
+
+                osSearcher.Dispose();
+            }
+            catch
+            {
+                // если WMI не отвечает
+            }
+
+            try
+            {
+                ManagementObjectSearcher memSearcher = new ManagementObjectSearcher(
+                    "SELECT Manufacturer, Capacity, Speed FROM Win32_PhysicalMemory");
+
+                foreach (ManagementObject item in memSearcher.Get())
+                {
+                    RamStick stick = new RamStick();
+
+                    string mfr = item["Manufacturer"] as string;
+                    if (mfr != null)
+                    {
+                        stick.Brand = mfr.Trim();
+                    }
+
+                    object cap = item["Capacity"];
+                    if (cap != null)
+                    {
+                        stick.SizeBytes = Convert.ToInt64(cap);
+                    }
+
+                    object spd = item["Speed"];
+                    if (spd != null)
+                    {
+                        stick.Frequency = Convert.ToInt32(spd);
+                    }
+
+                    data.Sticks.Add(stick);
+                }
+
+                memSearcher.Dispose();
+            }
+            catch
+            {
+                // пропускаем
+            }
+
+            return data;
+        }
+    }
+}
