6655db99e6a7b7c04379ab4bebe34154794a08fa
ss <sasha69.tmk@gmail.com>
2026-04-19 19:20:01 +0300

    Создана модель данных процессора CpuData

---
+++ HardwareMonitor/Models/CpuData.cs
@@ -0,0 +1,23 @@
+﻿namespace HardwareMonitor
+{
+    public class CpuData
+    {
+        public string ProcessorName { get; set; }
+        public int PhysicalCores { get; set; }
+        public int LogicalCores { get; set; }
+        public int FrequencyMHz { get; set; }
+        public double CurrentLoad { get; set; }
+        public string Vendor { get; set; }
+        public string PlatformType { get; set; }
+
+        public CpuData()
+        {
+            ProcessorName = "Не определено";
+            Vendor = "Не определено";
+            PlatformType = "Не определено";
+        }
+
+        public string FrequencyGHz => $"{FrequencyMHz / 1000.0:F2} ГГц";
+        public string LoadText => $"{CurrentLoad:F1}%";
+    }
+}
